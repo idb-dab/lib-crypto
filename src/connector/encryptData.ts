6f0f6dcdc6b6856d40b0959d6ecca326883a9d54
pankaj0998 <pankajchaurasia9598@gmail.com>
2024-11-07 14:12:32 +0530

    Fix grammatical error

--- src/connector/encryptData.ts
+++ src/connector/encryptData.ts
@@ -4,7 +4,7 @@ import { generateAesKey } from "../utils/generateAesKey";
 import { encryptPayload } from "../utils/encryptPayload";
 
 
-export default async function encryptRequestData(requestBody: any, headers: Headers) {
+export async function encryptRequestData(requestBody: any, headers: Headers) {
     const p = await getPublicKey(headers);
     const responseData = JSON.parse(Buffer.from(atob(p)).toString());
     const publicKey = JSON.parse(JSON.stringify(responseData.publicKey)); //structuredClone can be used here
